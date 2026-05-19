9114b01d43cffaa8587b7ad03af8aadb4abb807f
Andressa-Brioschi <andressabrioschi@gmail.com>
2026-05-19 12:13:22 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,41 +2,46 @@ const express = require("express")
 
 const app = express()
 
-function formatarData() {
-    const agora = new Date()
+// contador de protocolo
+let protocoloAtual = require("crypto")
 
-    const dia = String(agora.getDate()).padStart(2, "0")
-    const mes = String(agora.getMonth() + 1).padStart(2, "0")
-    const ano = agora.getFullYear()
-    const hora = String(agora.getHours()).padStart(2, "0")
-    const min = String(agora.getMinutes()).padStart(2, "0")
-    const seg = String(agora.getSeconds()).padStart(2, "0")
-
-    return `${dia}-${mes}-${ano} ${hora}:${min}:${seg}`
-}
-
-// permite receber JSON grande (CT-e vem grande em base64)
+// permite receber JSON grande
 app.use(express.json({ limit: "10mb" }))
+
+// função para formatar data
+function formatarDataHora(data) {
+    const dia = String(data.getDate()).padStart(2, "0")
+    const mes = String(data.getMonth() + 1).padStart(2, "0")
+    const ano = data.getFullYear()
+
+    const hora = String(data.getHours()).padStart(2, "0")
+    const minuto = String(data.getMinutes()).padStart(2, "0")
+    const segundo = String(data.getSeconds()).padStart(2, "0")
+
+    return `${dia}-${mes}-${ano} ${hora}:${minuto}:${segundo}`
+}
 
 // rota de teste
 app.get("/", (req, res) => {
     res.send("API Webhook funcionando")
 })
 
-// rota para webhook
+// rota webhook
 app.post("/webhook/cte", (req, res) => {
-    console.log("Webhook recebido")
-    console.log(req.body)
 
     try {
-        // aqui você pode processar o XML base64 depois
+
+        console.log("Webhook recebido")
+
+        console.log(req.body)
 
         const resposta = [
             {
                 retorno: {
                     codigo: 200,
-                    descricao: "ENVIO PROCESSADO COM SUCESSO",
-                    dataHora: formatarData()
+                    descricao: "OCORRENCIA PROCESSADA COM SUCESSO",
+                    dataHora: formatarDataHora(new Date()),
+                    protocolo: protocoloAtual.randomUUID()
                 }
             }
         ]
@@ -44,6 +49,7 @@ app.post("/webhook/cte", (req, res) => {
         res.status(200).json(resposta)
 
     } catch (error) {
+
         console.error(error)
 
         const respostaErro = [
@@ -51,7 +57,8 @@ app.post("/webhook/cte", (req, res) => {
                 retorno: {
                     codigo: 400,
                     descricao: "NAO FOI POSSIVEL PROCESSAR A REQUISICAO",
-                    dataHora: new Date().toLocaleString("pt-BR")
+                    dataHora: formatarDataHora(new Date()),
+                    protocolo: protocoloAtual.randomUUID()
                 }
             }
         ]
